ed0e18601edf340c7acf69e42e402edd02e4fbf2
Brunno Oliveira <brunnokick@gmail.com>
2021-04-26 18:40:54 -0300

    ajuste base

--- package/model.py
+++ package/model.py
@@ -15,9 +15,7 @@ logging.basicConfig(
 
 class Model:
     def __init__(self):
-        root_path = root_path = os.path.dirname(
-            os.path.dirname(os.path.dirname(__file__))
-        )
+        root_path = root_path = os.path.dirname(os.path.dirname(__file__))
         self.data_path = os.path.join(root_path, "data")
         self.model = None
         self.df: pd.DataFrame = None
@@ -26,20 +24,27 @@ class Model:
         self.X_test = None
         self.y_test = None
         self.predicted = None
+        self.tickers = None
 
     def load_data(self, df: pd.DataFrame = None):
         if df is None:
             self.df = pd.read_parquet(os.path.join(self.data_path, 'df_consolidado.parquet'))
+            self.tickers = self.df.iloc[:, -1:]
             self.df = self.df.iloc[:, :-1].copy()  # Remove ticker column
         else:
             self.df = df
+        max_date = self.df['date'].max()
+        # Previsao para o ultimo dia valido, removendo a primeira coluna (TARGET)
+        self.X_train = self.df[self.df['date'] < max_date].iloc[:, 1:]
+        self.y_train = self.df[self.df['date'] < max_date].iloc[:, 0]
+
+        self.X_test = self.df[self.df['date'] == max_date].iloc[:, 1:]
+        self.y_test = self.df[self.df['date'] == max_date].iloc[:, 0]        
 
     def set_model(self):
         logging.info("Start")
         self.model = DecisionTreeRegressor(random_state=42)
         return self.model
-
-    def 
 
     def fit_and_predict(self):
         logging.info("Start")
@@ -76,5 +81,6 @@ if __name__ == "__main__":
     root_path = root_path = os.path.dirname(os.path.dirname(__file__))
     model = Model()
     model.set_model()
+    model.load_data()
     model.fit_and_predict()
     model.plot_metrics()
