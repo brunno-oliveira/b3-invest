38afa7eb0f08302390901b7b4095a5df2df11a06
Brunno Oliveira <brunnokick@gmail.com>
2021-06-17 20:53:06 -0300

    add transform setor module

--- package/etl/transform/transform_setor.py
+++ package/etl/transform/transform_setor.py
@@ -9,7 +9,7 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
         logging.StreamHandler(),
-        logging.FileHandler(filename="data/log/transform.log"),
+        logging.FileHandler(filename="data/log/transform_setor.log"),
     ],
 )
 
@@ -20,19 +20,17 @@ class Transform:
             os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
         )
         data_path = os.path.join(root_path, "data")
-        self.history_path = os.path.join(data_path, "history")
         self.ticker_setorial_path = os.path.join(data_path, "b3_setorial.csv")
         self.setor_path = os.path.join(data_path, "setor.json")
         self.subsetor_path = os.path.join(data_path, "subsetor.json")
         self.segmento_path = os.path.join(data_path, "segmento.json")
 
-        self.consolidado: pd.DataFrame = None
         self.df_setorial: pd.DataFrame = None
         self.setor_map: pd.DataFrame = None
         self.subsetor_map: pd.DataFrame = None
         self.segmento_map: pd.DataFrame = None
 
-        self.output_consolidado_path = os.path.join(data_path, "df_consolidado.parquet")
+        self.output_consolidado_path = os.path.join(data_path, "df_setor.parquet")
 
     def transform(self):
         logging.info("Start")
@@ -40,12 +38,9 @@ class Transform:
         self.transform_setor()
         self.transform_subsetor()
         self.transform_segmento()
-        self.join_files()
-        self.merge_setorial_consolidado()
-        self.transform_ticker()
-        self.consolidado.to_parquet(self.output_consolidado_path)
+        self.df_setorial.to_parquet(self.output_consolidado_path)
         logging.info("Finished")
-        return self.consolidado
+        return self.df_setorial
 
     def load_data(self):
         logging.info("Start")
@@ -107,60 +102,5 @@ class Transform:
 
         self.df_setorial.drop(columns="listagem_segmento", inplace=True)
 
-    def join_files(self, target: str = "close") -> pd.DataFrame:
-        logging.info("Start")
-        df_list = []
-        for file in os.listdir(self.history_path):
-            if ".parquet" in file:
-                file_path = os.path.join(self.history_path, file)
-                df_list.append(pd.read_parquet(file_path))
-        self.consolidado = pd.concat(df_list)
-        self.consolidado = self.consolidado.reset_index()
-        self.consolidado.columns = [
-            "date",
-            "open",
-            "high",
-            "low",
-            "close",
-            "adj_close",
-            "volume",
-            "ticker",
-        ]
 
-        self.consolidado["codigo"] = self.consolidado["ticker"].str.slice(0, 4)
-        self.consolidado = self.consolidado[[target, "date", "ticker", "codigo"]].copy()
-
-        logging.info(f"self.consolidado.shape: {self.consolidado.shape}")
-
-    def merge_setorial_consolidado(self):
-        logging.info("Start")
-        self.consolidado = self.consolidado.merge(
-            self.df_setorial, how="inner", left_on="codigo", right_on="codigo"
-        )
-
-        self.consolidado = self.consolidado.sort_values(by=["date"], ascending=True)
-        self.consolidado["date"] = self.consolidado["date"].astype(str)
-        self.consolidado["date"] = self.consolidado["date"].str.replace("-", "")
-        self.consolidado["date"] = self.consolidado["date"].astype(int)
-
-        self.consolidado.reset_index(drop=True, inplace=True)
-
-        logging.info(f"self.consolidado.shape: {self.consolidado.shape}")
-
-    def transform_ticker(self):
-        logging.info("Start")
-        ticker_dummies = pd.get_dummies(
-            self.consolidado.ticker, prefix="ticker", prefix_sep="."
-        )
-        self.consolidado = pd.concat([self.consolidado, ticker_dummies], axis=1)
-
-        # Movendo a coluna codigo para ultima posicao
-        self.consolidado = self.consolidado.drop(columns=["codigo"])
-        df_ticker = self.consolidado.pop("ticker")
-        self.consolidado["ticker"] = df_ticker
-        self.consolidado = self.consolidado.dropna()
-
-
-if __name__ == "__main__":
-    df = Transform().transform()
-    print(df.shape)
+Transform().transform()
