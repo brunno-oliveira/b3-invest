c97752d16a1c2fc6b53d45ecb2f490c2d3e82290
Brunno Oliveira <brunnokick@gmail.com>
2021-06-17 21:39:49 -0300

    transform setor module

--- package/etl/transform/transform_setor.py
+++ package/etl/transform/transform_setor.py
@@ -2,6 +2,7 @@ import os
 import json
 import logging
 import pandas as pd
+from unidecode import unidecode
 
 logging.basicConfig(
     level=logging.INFO,
@@ -21,15 +22,7 @@ class Transform:
         )
         data_path = os.path.join(root_path, "data")
         self.ticker_setorial_path = os.path.join(data_path, "b3_setorial.csv")
-        self.setor_path = os.path.join(data_path, "setor.json")
-        self.subsetor_path = os.path.join(data_path, "subsetor.json")
-        self.segmento_path = os.path.join(data_path, "segmento.json")
-
         self.df_setorial: pd.DataFrame = None
-        self.setor_map: pd.DataFrame = None
-        self.subsetor_map: pd.DataFrame = None
-        self.segmento_map: pd.DataFrame = None
-
         self.output_consolidado_path = os.path.join(data_path, "df_setor.parquet")
 
     def transform(self):
@@ -44,15 +37,6 @@ class Transform:
 
     def load_data(self):
         logging.info("Start")
-        with open(self.setor_path, encoding="utf-8") as json_file:
-            self.setor_map = json.load(json_file)["setor_map"]
-
-        with open(self.subsetor_path, encoding="utf-8") as json_file:
-            self.subsetor_map = json.load(json_file)["subsetor_map"]
-
-        with open(self.segmento_path, encoding="utf-8") as json_file:
-            self.subsetor_map = json.load(json_file)["segmento_map"]
-
         self.df_setorial = pd.read_csv(
             self.ticker_setorial_path, sep=";", encoding="latin"
         )
@@ -62,7 +46,7 @@ class Transform:
         Criando as colunas dummies para SETOR
         """
         logging.info("Start")
-        self.df_setorial["setor"] = self.df_setorial["setor"].map(self.setor_map)
+        self.df_setorial = self.transform_column(self.df_setorial, "setor")
 
         setor_dummies = pd.get_dummies(
             self.df_setorial.setor, prefix="setor", prefix_sep="."
@@ -77,9 +61,7 @@ class Transform:
         Criando as colunas dummies para SUBSETOR
         """
         logging.info("Start")
-        self.df_setorial["subsetor"] = self.df_setorial["subsetor"].map(
-            self.subsetor_map
-        )
+        self.df_setorial = self.transform_column(self.df_setorial, "subsetor")
         setor_dummies = pd.get_dummies(
             self.df_setorial.subsetor, prefix="subsetor", prefix_sep="."
         ).drop(columns="subsetor.outros")
@@ -92,7 +74,7 @@ class Transform:
         Criando as colunas dummies para SEGMENTO
         """
         logging.info("Start")
-        self.df_setorial["segmento"] = self.df_setorial["segmento"].map(self.setor_map)
+        self.df_setorial = self.transform_column(self.df_setorial, "segmento")
         setor_dummies = pd.get_dummies(
             self.df_setorial.segmento, prefix="segmento", prefix_sep="."
         ).drop(columns="segmento.outros")
@@ -101,6 +83,21 @@ class Transform:
         )
 
         self.df_setorial.drop(columns="listagem_segmento", inplace=True)
+
+    @staticmethod
+    def transform_column(df: pd.DataFrame, col: str) -> pd.DataFrame:
+        """Padroniza os valores das colunas com:
+        unidecode, lower, replace , e ' '
+        """
+
+        def replace_method(x: str) -> str:
+            x = unidecode(x).lower()
+            x = x.replace(",", "")
+            x = x.replace(" ", "_")
+            return x
+
+        df[col] = df[col].apply(lambda x: replace_method(x))
+        return df
 
 
 Transform().transform()
