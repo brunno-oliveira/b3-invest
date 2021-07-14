f400ee8ef64bad2fd15e7c65749d8311ece969cd
Brunno Oliveira <brunnokick@gmail.com>
2021-07-13 21:03:54 -0300

    Add metrics and wandb integration

--- package/models/linear_regressor.py
+++ package/models/linear_regressor.py
@@ -2,10 +2,13 @@ from sklearn.linear_model import LinearRegression
 from model_base import ModelBase
 import logging
 
+GROUP_NAME = "LinearRegression"
+MODEL_NAME = "1.0"
+
 
 class ModelLinearRegression(ModelBase):
     def __init__(self):
-        super().__init__()
+        super().__init__(group_name=GROUP_NAME, model_name=MODEL_NAME)
 
     def set_model(self):
         logging.info("Start")
@@ -35,3 +38,4 @@ if __name__ == "__main__":
     model.load_data()
     model.fit_and_predict()
     model.plot_metrics()
+    model.plot_wandb()
--- package/models/model_base.py
+++ package/models/model_base.py
@@ -1,45 +1,66 @@
-from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+from sklearn.metrics import (
+    mean_absolute_percentage_error,
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+)
 import matplotlib.pyplot as plt
 import datetime as dt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import logging
+import wandb
 import os
+
+sns.set_theme(style="darkgrid")
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(process)-5d][%(asctime)s][%(filename)-10s][%(funcName)-20s][%(levelname)-5s] %(message)s",
+    format="[%(process)-5d][%(asctime)s][%(filename)-10s][%(funcName)-10s][%(levelname)-5s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[logging.StreamHandler(), logging.FileHandler(filename="extract.log")],
 )
 
 
 class ModelBase:
-    def __init__(self):
+    def __init__(self, group_name: str, model_name: str):
         root_path = root_path = os.path.dirname(
             os.path.dirname(os.path.dirname(__file__))
         )
         self.data_path = os.path.join(root_path, "data")
         self.model = None
         self.df: pd.DataFrame = None
-        self.X_train = None
-        self.y_train = None
-        self.X_test = None
-        self.y_test = None
-        self.predicted = None
-        self.tickers = None
+        self.X_train: pd.DataFrame = None
+        self.y_train: pd.Series = None
+        self.X_tes: pd.DataFramet = None
+        self.y_test: pd.Series = None
+        self.y_data: np.ndarray = None
+        self.predicted: np.ndarray = None
 
-    def load_data(self, df: pd.DataFrame = None):
-        if df is None:
-            self.df = pd.read_parquet(
-                os.path.join(self.data_path, "df_consolidado.parquet")
-            )
+        # Metrics
+        self.mape_score: float = None
+        self.mae_score: float = None
+        self.mse_score: float = None
+        self.r_square_score: float = None
 
-            # Coluna para facilitar a busca por tickers
-            self.tickers = self.df.iloc[:, 0]
-            self.df = self.df.iloc[:, 2:].copy()  # Remove ticker columns
-        else:
-            self.df = df
+        wandb.init(
+            project="b3-invest",
+            entity="brunno-oliveira",
+            group=group_name,
+            name=model_name,
+        )
+
+    def load_data(self):
+        logging.info("Start")
+        self.df = pd.read_parquet(
+            os.path.join(self.data_path, "df_consolidado.parquet")
+        )
+
+        # Coluna para facilitar a busca por tickers
+        tickers = self.df.iloc[:, 0]
+        self.df = self.df.iloc[:, 2:].copy()  # Remove ticker columns
+
         max_date = self.df["date"].max()
         # Previsao para o ultimo dia valido, removendo a primeira coluna (TARGET)
         self.X_train = self.df[self.df["date"] < max_date].iloc[:, 1:]
@@ -47,6 +68,14 @@ class ModelBase:
 
         self.X_test = self.df[self.df["date"] == max_date].iloc[:, 1:]
         self.y_test = self.df[self.df["date"] == max_date].iloc[:, 0]
+
+        # Devolvendo a coluna de ticker
+        self.df["ticker"] = tickers
+
+        # DF para facilitar a validacao
+        self.y_data = self.df[self.df["date"] == self.df["date"].max()][
+            ["ticker", "date", "close"]
+        ].tail(len(self.X_test))
 
         self.transform_date()
 
@@ -65,21 +94,49 @@ class ModelBase:
     def predict(self):
         pass
 
+    def plot_wandb(self):
+        if None in [
+            self.mape_score,
+            self.mae_score,
+            self.mse_score,
+            self.r_square_score,
+        ]:
+            error = "Error: Empty Metrics. Run plot_metrics before plot_wandb"
+            logging.error(error)
+            raise Exception(error)
+        wandb.log(
+            {
+                "mape_score": self.mape_score,
+                "mae_score": self.mae_score,
+                "mse_score": self.mse_score,
+                "r2_score": self.r_square_score,
+            }
+        )
+        wandb.finish()
+
     def transform_date(self):
         """O modelo nao trabalha com o tipo datetime"""
         self.X_train["date"] = self.X_train["date"].map(dt.datetime.toordinal)
         self.X_test["date"] = self.X_test["date"].map(dt.datetime.toordinal)
 
-    def plot_metrics(self):
-        logging.info(f"r2_score : {round(r2_score(self.predicted, self.y_test),4)}")
-        logging.info(
-            f"mean_squared_error: {round(mean_squared_error( self.predicted, self.y_test, squared=False),4)}"
+    def plot_metrics(self, plot_graph: bool = False):
+        self.mape_score = round(
+            mean_absolute_percentage_error(self.predicted, self.y_test), 4
         )
-        logging.info(
-            f"mean_absolute_error: {round(mean_absolute_error( self.predicted, self.y_test),4)}"
-        )
+        self.mae_score = round(mean_absolute_error(self.predicted, self.y_test), 4)
+        self.mse_score = round(mean_squared_error(self.predicted, self.y_test), 4)
+        self.r_square_score = round(r2_score(self.predicted, self.y_test), 4)
 
-        fig, ax = plt.subplots(figsize=(25, 8))
-        ax.plot(self.predicted)
-        ax.plot(np.array(self.y_test))
-        plt.show()
+        logging.info(f"mape_score : {self.mape_score}")
+        logging.info(f"mae_score : {self.mae_score}")
+        logging.info(f"mse_score : {self.mse_score}")
+        logging.info(f"r2_score : {self.r_square_score}")
+
+        if plot_graph:
+            fig, ax = plt.subplots(figsize=(30, 6))
+
+            sns.lineplot(x=list(self.y_data["ticker"]), y=list(self.y_data["close"]))
+            ax.plot(self.predicted)
+            plt.xticks(rotation=90)
+
+            plt.show()
