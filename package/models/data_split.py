e9970fa5325b86de449cfb653b7464efb7acc184
Brunno Oliveira <brunnokick@gmail.com>
2021-09-08 09:27:19 -0300

    save best params

--- package/models/data_split.py
+++ package/models/data_split.py
@@ -11,6 +11,15 @@ class DataSplit:
         self.X_train: pd.DataFrame = None
         self.y_train: pd.Series = None
 
+        self.X_test_1_day: pd.Series = None
+        self.y_test_1_day: pd.Series = None
+
+        self.X_test_7_day: pd.Series = None
+        self.y_test_7_day: pd.Series = None
+
+        self.X_test_14_day: pd.Series = None
+        self.y_test_14_day: pd.Series = None
+
         self.X_test_28_days: pd.Series = None
         self.y_test_28_days: pd.Series = None
 
@@ -22,3 +31,7 @@ class DataSplit:
         self.X_test = self.df[self.df["date"] > TRAIN_MAX_DATE].iloc[:, 1:]
         self.y_test = self.df[self.df["date"] > TRAIN_MAX_DATE].iloc[:, 0]
         self.y_data = self.df[self.df["date"] > TRAIN_MAX_DATE]
+
+        self.X_test_28_days = self.df[self.df["date"] > TRAIN_MAX_DATE].iloc[:, 1:]
+        self.y_test_28_days = self.df[self.df["date"] > TRAIN_MAX_DATE].iloc[:, 0]
+        self.y_data_28_days = self.df[self.df["date"] > TRAIN_MAX_DATE]
--- package/models/model_base.py
+++ package/models/model_base.py
@@ -53,9 +53,7 @@ class ModelBase(DataSplit):
         self.gs: GridSearchCV = None
         self.gs_params: Dict = None
         self.gs_result: DataFrame = None
-        self.gs_best_params_path = os.path.join(
-            self.model_path, 'best_params.json')
-
+        self.gs_best_params_path = os.path.join(self.model_path, "best_params.json")
 
         # Metrics
         self.mape_score: float = None
@@ -100,14 +98,35 @@ class ModelBase(DataSplit):
         self.gs.fit(self.X_train, self.y_train)
 
         # Save results
-        log.info('Saving GridSearch results and best params..')
+        log.info("Saving GridSearch results and best params..")
         self.gs_result = pd.DataFrame(self.gs.cv_results_)
+
+        round_columns = [
+            "mean_fit_time",
+            "std_fit_time",
+            "mean_score_time",
+            "std_score_time",
+            "split0_test_score",
+            "split1_test_score",
+            "split2_test_score",
+            "split3_test_score",
+            "split4_test_score",
+            "mean_test_score",
+            "std_test_score",
+        ]
+
+        for col in round_columns:
+            self.gs_result = round(self.gs_result, 4)
+
         self.gs_result.to_csv(os.path.join(self.model_path, "gs_results.csv"))
 
         # Save best params
         gs_best_params = json.dumps(self.gs.best_params_)
         with io.open(self.gs_best_params_path, "w") as f:
             f.write(gs_best_params)
+
+    def grid_search_splitter(self):
+        log.info("Start")
 
     @property
     @abstractmethod
