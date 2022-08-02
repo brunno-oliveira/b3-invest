4532e31b6077f2e6b84138a760a89501126ef390
Brunno Oliveira <brunnokick@gmail.com>
2022-08-02 17:50:08 -0300

    1500 estimator only

--- package/models/model_grid_search.py
+++ package/models/model_grid_search.py
@@ -55,10 +55,9 @@ class GridSearch(DataSplit):
             estimator=self.model.__class__(),
             param_grid=self.gs_params,
             cv=self.train_test_index(),
-            n_jobs=-2,
+            n_jobs=-1,
             scoring="neg_root_mean_squared_error",
             verbose=2,
-            pre_dispatch=16,
         )
 
         self.gs.fit(self.X_gs_train, self.y_gs_train)
